6cee5459532b630af22379a9068814df8c7bbe3f
Nhsdkk <38581029+Nhsdkk@users.noreply.github.com>
2023-11-17 22:50:25 +0300

    fix: errors with singleton on Csv Logger fix

--- logger/src/csv_logger_impl.py
+++ logger/src/csv_logger_impl.py
@@ -1,79 +1,61 @@
-from pandas import read_csv
-from numpy import zeros
+import pandas
 from typing import Self
 
-from .logger_interface import LoggerInterface
-from shared.constructor_locker import ConstructorLocker
+
+from logger.src.logger_interface import LoggerInterface
+from shared.debugger import debug
+from shared.singleton import singleton
 
 
-class CsvLogger(LoggerInterface, metaclass=ConstructorLocker):
+@singleton
+class CsvLogger(LoggerInterface):
     """Class for logger, which outputs to CSV file"""
 
-    _column_name_last_index_mapping: dict[str, int] = {}
+    _columns: dict[str, list[float]] = {}
     _filename: str = None
-    _instance = None
+    _instance: Self = None
 
     def __init__(self, filename: str):
         """
-        Private constructor
+        Public constructor
 
         :param filename: name of the file to write
         """
         self._filename = filename
 
-    def _write_fs(self, column_name: str, value: float, column_exists: bool = True):
+    def _clean_data(self) -> None:
+        """
+        Ensure that all column arrays have the same size by appending zeroes at the end
+
+        :return: None
+        """
+        max_length = max([len(self._columns[key]) for key in self._columns.keys()])
+
+        for key in self._columns.keys():
+            self._columns[key] += [0] * (max_length - len(self._columns[key]))
+
+        return
+
+    def dump(self):
         """
         Write data to csv file
 
-        :param column_name: name of the column
-        :param value: value of the variable
-        :param column_exists: flag to check for column existence
+        :return: None
         """
-        with open(self._filename, "r") as file:
-            df = read_csv(file)
-
-        if not column_exists:
-            if len(df.columns) == 0:
-                df.insert(0, column_name, value, allow_duplicates=True)
-                self._column_name_last_index_mapping[column_name] = 1
-            else:
-                rows_length = df.shape[0]
-                df.insert(
-                    len(df.columns),
-                    column_name,
-                    zeros(rows_length, float),
-                    allow_duplicates=True,
-                )
-                self._column_name_last_index_mapping[column_name] = rows_length + 1
-        else:
-            index = self._column_name_last_index_mapping[column_name]
-            rows_length = df.shape[0]
-            if index <= rows_length:
-                df.set_value(index, column_name, value)
-            else:
-                df.loc[index] = zeros(len(df.columns))
-                df.set_value(index, column_name, value)
-
+        self._clean_data()
+        df = pandas.DataFrame.from_dict(self._columns)
         df.to_csv(self._filename, index=False, mode="a")
 
+    @debug
     def log(self, variable_type: str, value: float) -> None:
         """
-        Log the value of the variable
+        Save value of the variable
 
-        :param variable_type: the variable group name to which this value will be assigned
-        :param value: value of the variable
+        :param variable_type: Type, that allows us to assign variable to the group
+        :param value: Value, that need to be saved
+        :return: None
         """
-        self._write_fs(
-            variable_type, value, variable_type in self._column_name_last_index_mapping
-        )
-
-    @classmethod
-    def create(cls, filename: str = "out.csv") -> Self:
-        """
-        Public constructor
-
-        :param filename: name of the file to write (by default "log.csv")
-        :return: CsvLogger class instance
-        """
-        cls._filename = filename
-        return cls
+        if variable_type not in self._columns:
+            self._columns[variable_type] = [value]
+        else:
+            self._columns[variable_type].append(value)
--- logger/src/logger_interface.py
+++ logger/src/logger_interface.py
@@ -1,14 +1,20 @@
-from shared.constructor_locker import ConstructorLocker
-
-
-class LoggerInterface(metaclass=ConstructorLocker):
+class LoggerInterface:
     """Interface for logger"""
 
     def log(self, variable_type: str, value: float) -> None:
         """
-        Log the value of the variable
+        Save value of the variable
 
         :param variable_type: Type, that allows us to assign variable to the group
-        :param value: Value, that need to be logged
+        :param value: Value, that need to be saved
+        :return: None
+        """
+        pass
+
+    def dump(self) -> None:
+        """
+        Dump log content
+
+        :return: None
         """
         pass
