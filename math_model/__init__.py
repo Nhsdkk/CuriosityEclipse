a9e88b994dcda6ff4fe8e9db1f01ee29c6c56422
Nhsdkk <38581029+Nhsdkk@users.noreply.github.com>
2023-11-19 16:57:57 +0300

    fix: docstrings added and methods privated

--- math_model/__init__.py
+++ math_model/__init__.py
@@ -1,2 +1,5 @@
+from .src.dif_equation_solver import Equation
+
 if __name__ == "__main__":
-    pass
+    trier = Equation(requested_flight_time=400)
+    trier.solve()
--- math_model/src/dif_equation_solver.py
+++ math_model/src/dif_equation_solver.py
@@ -2,6 +2,7 @@ from typing import Union
 import math
 import numpy as np
 import matplotlib.pyplot as plt
+import constants
 
 
 class Equation:
@@ -12,7 +13,6 @@ class Equation:
         h: Union[float, int] = 1e-5,
         k_fuel: Union[float, int] = 1510.84,
         mass_of_spaceship_with_fuel: Union[float, int] = 539_287,
-        c_x: Union[float, int] = 0.5,
         volume: Union[float, int] = 706.86,
         pressure_0: Union[float, int] = 1e5,
         p_thrust: Union[float, int] = 9.271 * 1e6,
@@ -26,18 +26,11 @@ class Equation:
         :param h: Step between two values in approximations
         :param k_fuel: Fuel usage constant expressed in kilograms / second (const)
         :param mass_of_spaceship_with_fuel: Full fuel mass + mass of spaceship without fuel expressed in kilograms
-        :param c_x: Coefficient of aerodynamic drag (const)
         :param volume: Volume of spaceship expressed in metres ^ 3
         :param pressure_0: Air pressure on start expressed in pascals
         :param p_thrust: Thrust force expressed in newtons
         :param temperature_0: Air temperature on start expressed in kelvin degrees
         """
-        self.G = 6.6743 * 1e-11
-        self.EarthMass = 5.9742 * 1e24
-        self.R = 8.31
-        self.EarthRadius = 6.371 * 1e6
-        self.AirMolarMass = 28.98 * 1e-3
-        self.c_x = c_x
         self.h = h
         self.k_fuel = k_fuel
         self.p_thrust = p_thrust
@@ -45,8 +38,8 @@ class Equation:
         self.r0 = r0
         self.r1 = (
             (
-                self.p_thrust / self.find_mass(self.h)
-                - self.G * self.EarthMass / self.EarthRadius**2
+                self.p_thrust / self._find_mass(self.h)
+                - constants.G * constants.EARTH_MASS / constants.EARTH_RADIUS**2
             )
             * self.h**2
             / 2
@@ -56,7 +49,7 @@ class Equation:
         self.n = int(requested_flight_time / self.h)
         self.temperature_0 = temperature_0
 
-    def find_mass(self, current_flight_time: Union[float, int]) -> Union[float, int]:
+    def _find_mass(self, current_flight_time: Union[float, int]) -> Union[float, int]:
         """
         Finding mass of the spaceship at given flight time
 
@@ -68,16 +61,17 @@ class Equation:
         )
         return temporary_mass if temporary_mass > 37_736 else 37_736
 
-    def find_g(self, r: Union[float, int]) -> Union[float, int]:
+    @staticmethod
+    def _find_g(r: Union[float, int]) -> Union[float, int]:
         """
         Finding acceleration of free fall on given height
 
         :param r: Height expressed in meters
         :return: Acceleration of free fall on given height expressed in meters / second ^ 2
         """
-        return self.G * self.EarthMass / (self.EarthRadius + r) ** 2
+        return constants.G * constants.EARTH_MASS / (constants.EARTH_RADIUS + r) ** 2
 
-    def find_temperature(self, r: Union[float, int]) -> Union[float, int]:
+    def _find_temperature(self, r: Union[float, int]) -> Union[float, int]:
         """
         Finding air temperature on given height
 
@@ -87,7 +81,7 @@ class Equation:
         temporary_temperature = self.temperature_0 - 0.003 * r
         return 173 if temporary_temperature < 173 else temporary_temperature
 
-    def find_pressure(self, r: Union[float, int]) -> Union[float, int]:
+    def _find_pressure(self, r: Union[float, int]) -> Union[float, int]:
         """
         Finding air pressure on given height
 
@@ -95,13 +89,13 @@ class Equation:
         :return: Air pressure expressed in pascals
         """
         return self.pressure_0 * math.e ** (
-            -self.AirMolarMass
-            * self.find_g(r)
+            -constants.AIR_MOLAR_MASS
+            * self._find_g(r)
             * r
-            / (self.R * self.find_temperature(r))
+            / (constants.R * self._find_temperature(r))
         )
 
-    def find_ro(self, r: Union[float, int]) -> Union[float, int]:
+    def _find_ro(self, r: Union[float, int]) -> Union[float, int]:
         """
         Finding air density on given height
 
@@ -109,71 +103,122 @@ class Equation:
         :return: Air density expressed in kilograms / metre ^ 3
         """
         return (
-            self.find_pressure(r)
-            * self.AirMolarMass
-            / (self.R * self.find_temperature(r))
+            self._find_pressure(r)
+            * constants.AIR_MOLAR_MASS
+            / (constants.R * self._find_temperature(r))
         )
 
-    def fi(
+    def _fi(
         self, r: Union[float, int], current_flight_time: Union[float, int]
     ) -> Union[float, int]:
-        temp_thrust = self.p_thrust / self.find_mass(current_flight_time)
-        if current_flight_time > 311.3:
-            return -self.G * self.EarthMass / (self.EarthRadius + r) ** 2
-        else:
-            return temp_thrust - self.G * self.EarthMass / (self.EarthRadius + r) ** 2
+        """
+        Find gravitational force coefficient for differential equation.
 
-    def w(
+        :param r: Height above sea level in meters
+        :param current_flight_time: Current time starting from the rocket launch in seconds
+        :return: Gravitational force coefficient
+        """
+        temp_thrust = self.p_thrust / self._find_mass(current_flight_time)
+        if current_flight_time > 311.3:
+            return (
+                -constants.G * constants.EARTH_MASS / (constants.EARTH_RADIUS + r) ** 2
+            )
+        else:
+            return (
+                temp_thrust
+                - constants.G * constants.EARTH_MASS / (constants.EARTH_RADIUS + r) ** 2
+            )
+
+    def _w(
         self, r: Union[float, int], current_flight_time: Union[float, int]
     ) -> Union[float, int]:
+        """
+        Finding air resistance force coefficient for differential equation.
+
+        :param r: Height above sea level in meters
+        :param current_flight_time: Current time starting from the rocket launch in seconds
+        :return: Air resistance force coefficient
+        """
         if r > 100_000:
             return 0
         return (
-            -self.c_x
+            -constants.C_X
             * self.volume ** (2 / 3)
-            * self.find_ro(r)
-            / (2 * self.find_mass(current_flight_time))
+            * self._find_ro(r)
+            / (2 * self._find_mass(current_flight_time))
         )
 
-    def find_roots_neat_approximation(
+    def _find_roots_neat_approximation(
         self,
         current_flight_time: Union[float, int],
         r_i_minus1: Union[float, int],
         r_i: Union[float, int],
     ) -> tuple[Union[float, int, complex], Union[float, int, complex]]:
-        # Написать про то, что считается уравнение вида ax^2 + bx + c = 0
-        w_i, fi_i = self.w(r_i, current_flight_time), self.fi(r_i, current_flight_time)
+        """
+        Find roots of the equation (ax^2 + bx + c = 0) precisely.
+
+        :param current_flight_time: Current time starting from the rocket launch in seconds
+        :param r_i_minus1: Previous height in meters
+        :param r_i: Current height in meters
+        :return: 2 roots of the equation (ax^2 + bx + c = 0)
+        """
+        w_i, fi_i = self._w(r_i, current_flight_time), self._fi(
+            r_i, current_flight_time
+        )
         a = w_i / 4
         b = -1 - r_i_minus1 * w_i / 2
         c = 2 * r_i - r_i_minus1 + w_i * r_i_minus1**2 / 4 + self.h**2 * fi_i
         return np.roots(np.array([a, b, c]))
 
-    def find_root_rough_approximation(
+    def _find_root_rough_approximation(
         self,
         current_flight_time: Union[float, int],
         r_i_minus1: Union[float, int],
         r_i: Union[float, int],
     ) -> Union[float, int]:
-        w_i, fi_i = self.w(r_i, current_flight_time), self.fi(r_i, current_flight_time)
+        """
+        Find roots of the equation (ax^2 + bx + c = 0) with worse precision.
+
+        :param current_flight_time: Current time starting from the rocket launch in seconds
+        :param r_i_minus1: Previous height in meters
+        :param r_i: Current height in meters
+        :return: 2 roots of the equation (ax^2 + bx + c = 0)
+        """
+        w_i, fi_i = self._w(r_i, current_flight_time), self._fi(
+            r_i, current_flight_time
+        )
         return 2 * r_i - r_i_minus1 + self.h**2 * fi_i + w_i * (r_i - r_i_minus1) ** 2
 
-    def get_next_r(
+    def _get_next_r(
         self,
         current_flight_time: int,
         r_i_minus1: Union[float, int],
         r_i: Union[float, int],
     ) -> Union[float, int]:
+        """
+        Get next height in meters.
+
+        :param current_flight_time: Current time starting from the rocket launch in seconds
+        :param r_i_minus1: Previous height in meters
+        :param r_i: Current height in meters
+        :return: Next height in meters
+        """
         roots = tuple(
             item
-            for item in self.find_roots_neat_approximation(
+            for item in self._find_roots_neat_approximation(
                 current_flight_time, r_i_minus1, r_i
             )
             if item >= 0 and type(item) is not complex
         )
-        x = self.find_root_rough_approximation(current_flight_time, r_i_minus1, r_i)
+        x = self._find_root_rough_approximation(current_flight_time, r_i_minus1, r_i)
         return min(roots, key=lambda q: abs(q - x))
 
     def solve(self) -> None:
+        """
+        Main function to solve differential equation.
+
+        :return: None
+        """
         r_0 = self.r0
         r_1 = self.r1
         abscissa_array = np.zeros(self.n + 2, dtype="float64")
@@ -183,7 +228,7 @@ class Equation:
         for i in np.arange(1, self.n + 1):
             current_flight_time = i * self.h
             abscissa_array[i] = current_flight_time
-            ordinate = self.get_next_r(current_flight_time, r_i_minus1=r_0, r_i=r_1)
+            ordinate = self._get_next_r(current_flight_time, r_i_minus1=r_0, r_i=r_1)
             ordinate_array[i + 1] = ordinate
             if not i % 100000:
                 with open("rocket_flight_stats.txt", "a+") as file:
@@ -202,7 +247,3 @@ class Equation:
         plt.grid()
         plt.plot(abscissa_array, ordinate_array)
         plt.show()
-
-
-trier = Equation(requested_flight_time=400)
-trier.solve()
