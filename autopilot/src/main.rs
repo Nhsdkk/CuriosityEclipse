5763f177e4445d6b7435f4c0b6c52fff9fcf4f59
Nhsdkk <nikittsev.anton@gmail.com>
2023-12-05 00:05:04 +0300

    feat: autopilot create

--- autopilot/src/main.rs
+++ autopilot/src/main.rs
@@ -1,3 +1,11 @@
+use crate::autopilot::Autopilot;
+
+mod autopilot;
+mod vector;
+mod point;
+
 fn main() {
     println!("Hello, world!");
+    let mut autopilot = Autopilot::new("127.0.0.1", 1000, 1001);
+    autopilot.land();
 }
